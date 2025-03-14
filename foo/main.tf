3afa3aeda7c937d64047930ef59088fbdb9ff90a
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-03-14 13:57:34 +0900

    ci: test#2431

---
+++ foo/main.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "example" {
+  name       = "test-tfaction"
+  visibility = "public"
+}
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.6.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = "suzuki-shunsuke"
+}
